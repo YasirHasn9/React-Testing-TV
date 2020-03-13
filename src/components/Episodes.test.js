13724beceb547e83eaebcfe3f1614779d68deef4
YasirHasn9 <hassanyasir223@gmail.com>
2020-03-13 13:17:46 -0700

    recoded some testing

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { render, wait } from "@testing-library/react";
+import { render, wait, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { fetchShow as mockFetchEpisodes } from "../api/fetchShow";
 import App from "../App";
-import {data as fakeDate} from "../mockTestData"
- 
+import { data as fakeDate } from "../mockTestData";
+
+import Episodes from "./Episodes";
+
 jest.mock("../api/fetchShow");
 
-test("select a season", // its a promise so it should a async
-async () => {
+test("select a season", async () => {
+  // its a promise so it should a async
   mockFetchEpisodes.mockResolvedValueOnce(fakeDate);
 
   const { getByText } = render(<App />);
@@ -17,9 +19,15 @@ async () => {
     getByText(/Select a season/i);
   });
   const dropDown = getByText(/select a season/i);
-  userEvent.click(dropDown);
+  act(() => {
+    userEvent.click(dropDown);
+  });
+
   const text = getByText(/season 1/i);
   expect(text).toBeInTheDocument();
-  userEvent.click(text);
+
+  act(() => {
+    userEvent.click(text);
+  });
   getByText(/season 1, episode 1/i);
 });
